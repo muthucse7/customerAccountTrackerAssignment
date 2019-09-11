0e83442e136dc5cebb31382e85809f85a3ad18fa
muthucse7 <muthu.softwar@gmail.com>
2019-09-10 22:47:31 -0400

    customerAccountTracker-milestone-3 project created Spring Boot with Hibernate JPA

---
+++ customerAccountTracker-milestone-3/src/main/java/com/wipro/bank/main/customerAccountTrackermilestone3/CustomerAccountTrackerMilestone3Application.java
@@ -0,0 +1,13 @@
+package com.wipro.bank.main.customerAccountTrackermilestone3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerAccountTrackerMilestone3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerAccountTrackerMilestone3Application.class, args);
+	}
+
+}
